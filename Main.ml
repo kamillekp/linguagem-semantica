35403243a2f21edd9522910f5cbfd1dc5a739d58
Kamille Konarzewski <kamille.kpimentel@gmail.com>
2025-06-05 18:37:32 -0300

    if step

--- Main.ml
+++ Main.ml
@@ -35,29 +35,29 @@ let rec typeInfer (e:expr) : tipo option =
   match e with 
   | Num _       -> Some TyInt
   | Bool _      -> Some TyBool
-  | Id _        -> Some string                        (* (NS) Identificadores não têm tipo definido, daí coloquei o tipo real. *) 
+  | Id _        -> None                        (* (NS) ID seria apenas um valor para (des)referenciar? *) 
 
   | If (e1,e2,e3) -> (
     match typeInfer e1 with
-      | Some Bool -> (
+      | Some TyBool -> (
         match (typeInfer e2,typeInfer e3) with
           | (Some t2, Some t3) -> if t2=t3 then Some t2 
                                   else None
         | _ -> None)
     | _ -> None)  
     
-  | Binop (op,e1,e2) -> (
+  | Binop (bop,e1,e2) -> (
     match (typeInfer e1, typeInfer e2) with
       | (Some TyInt, Some TyInt) -> (
-        match op with
+        match bop with
           | Sum | Sub | Mul | Div -> Some TyInt
-          | Eq  | Neq | Lt | Gt -> Some TyBool
+          | Eq  | Neq | Lt  | Gt  -> Some TyBool
           | _ -> None)
-      | (Some TyBool, Some TyBool) -> (
-        match op with
+      | (Some TyBool, Some TyBool) -> ( 
+        match bop with
           | And | Or -> Some TyBool
-          | _ -> None)
-      | _ -> None)
+          | _ -> None
+      | _ -> None))
 
   | Wh (e1, e2) -> (
     match typeInfer e1 with
@@ -68,23 +68,22 @@ let rec typeInfer (e:expr) : tipo option =
       | _ -> None)
 
   
-  | Asg (Id _, e1) -> (
-    match (typeInfer Id_,typeInfer e1) with
-      | (Some (TyRef t1), Some t2) -> if t1 = t2 then Some TyUnit 
-                                      else None
+  | Asg (e1, e2) -> (
+    match typeInfer e1 with
+      | Some TyRef t1 -> (
+        match typeInfer e2 with
+          | Some t2 ->  if t1 = t2 then Some TyUnit
+                        else None)
       | _ -> None)
 
-  | Let (var, t, e1, e2) -> (                         (* (NS) não tenho certeza se a ordem de verificação está correta.*)
-    match typeInfer var with
-      | Some string -> (
-        match (typeInfer t, typeInfer e1) with
-          | (Some t1, Some t2) -> if t1 = t2 then 
-                                    match typeInfer e2 with
-                                      | Some t3 -> Some t3
-                                      | _ -> None
-                                  else None
-          | _ -> None) 
-      | _ -> None)
+  | Let (string, tipo, e1, e2) -> (                         
+    match typeInfer e1 with
+      | Some t1 -> if t1 = tipo then                  
+                    match typeInfer e2 with
+                      | Some t2 -> Some t2
+                      | _ -> None
+                  else None
+      | _ -> None) 
 
   | New e1 -> (
     match typeInfer e1 with
@@ -122,31 +121,50 @@ let rec value (e:expr) : bool =
   | _ -> false
 
 
-(* step : expr -> expr option *)                      (* (NF) Ainda não comecei. *)
-let rec step (e:expr) : term option =  
+(* step : expr -> expr option *)                      
+let rec step (e:expr) : expr option =  
   match e with 
-  |
-;;
-                      
-(* steps : expr -> expr *)
-let rec steps (e:expr) : expr =
-  match step e with
-  | None -> e
-  | Some e' -> steps e'
+  | Num _ | Bool _ | Unit | Id _ -> None
+
+  | If (Bool true, e2, _) -> Some e2
+  | If (Bool false, _, e3) -> Some e3
+  | If (e1, e2, e3) -> 
+    match step e1 with
+      | Some e1' -> Some (If (e1', e2, e3))
+      | None -> None
+  
+  (*| Binop (Sum, Num v1, Num v2)   -> Some (Num (v1 + v2))    
+  | Binop (Sub, Num v1, Num v2)   -> Some (Num (v1 - v2))
+  | Binop (Mul, Num v1, Num v2)   -> Some (Num (v1 * v2))
+  | Binop (Div, Num v1, Num v2)   -> Some (Num (v1 / v2))
+  | Binop (Eq, Num v1, Num v2)    -> Some (Bool (v1 = v2))
+  | Binop (Neq, Num v1, Num v2)   -> Some (Bool (v1 <> v2))
+  | Binop (Lt, Num v1, Num v2)    -> Some (Bool (v1 < v2))
+  | Binop (Gt, Num v1, Num v2)    -> Some (Bool (v1 > v2))
+  | Binop (And, Bool b1, Bool b2) -> Some (Bool (b1 && b2))
+  | Binop (Or, Bool b1, Bool b2)  -> Some (Bool (b1 || b2))*)
+    
+  (**| Binop (bop, e1, e2) -> 
+      (match step e1 with
+      | Some e1' -> Some (Binop (bop, e1', e2))
+      | None -> None)
+
+  | Binop (bop, Num v1, e2) -> 
+      (match step e2 with
+      | Some e2' -> Some (Binop (bop, Num v1, e2'))
+      | None -> None)*)
+
   
 
-
-
-
-
-
-
+  
+;;
+                      
 
 (* ----------------------------------------------- *)
 (* Programa para calcular fatorial de um número    *)
 (* ----------------------------------------------- *)
 
-(* !z > 0 *)
+(*(* !z > 0 *)
 let cndwhi = Binop(Gt, Deref (Id "z"), Num 0)
 
 (* y := !y * !z *)
@@ -161,7 +179,7 @@ let whi = Wh(cndwhi, bdwhi)
 (* imprime o valor atual de y *)
 let prt = Print(Deref (Id "y"))
 (* Sequência de comandos: executa o while e depois imprime o resultado *)
-let seq = Seq(whi, prt)
+let seq = Seq(whi, prt)*)
 
 
 (* Outra forma de fazer a mesma coisa *)
