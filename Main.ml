6c626ff162b9a830e13487cb2d9bacf4864b9fa5
Kamille Konarzewski <kamille.kpimentel@gmail.com>
2025-06-05 11:01:22 -0300

    organização

--- Main.ml
+++ Main.ml
@@ -2,80 +2,72 @@
 type bop =  
   | Sum | Sub | Mul | Div   (* operações aritméticas *)
   | Eq  | Neq | Lt | Gt     (* operações relacionais *)
-  | And | Or                 (* operações lógicas *) 
+  | And | Or                (* operações lógicas *) 
 
 (* Definição dos tipos de dados *)
 type tipo = 
-  | TyInt                    (* inteiro *)
-  | TyBool                   (* booleano *)
-  | TyRef of tipo            (* referência a um tipo *)
-  | TyUnit                   (* tipo unitário, sem valor *)
+  | TyInt                   
+  | TyBool                   
+  | TyRef of tipo            
+  | TyUnit                   
 
 (* Definição das expressões da linguagem *)
 type expr = 
-  | Num of int               (* número inteiro *)
-  | Bool of bool            (* valor booleano *)
-  | Id of string            (* identificador de variável *)
-  | If of (expr * expr * expr)  (* condicional: if-then-else *)
-  | Binop of (bop * expr * expr) (* operação binária *)
-  | Wh of (expr * expr)     (* laço while: condição e corpo *)
-  | Asg of (expr * expr)    (* atribuição *)
-  | Let of (string * tipo * expr * expr)  (* declaração de variável: nome, tipo, valor, corpo *)
-  | New of expr             (* alocação de nova referência *)
-  | Deref of expr           (* desreferenciar uma referência *)
-  | Unit                    (* valor unitário *)
-  | Seq of expr * expr      (* sequência de duas expressões *)
-  | Read                    (* comando de leitura *)
-  | Print of expr           (* comando de impressão *)
+  | Num of int               
+  | Bool of bool            
+  | Id of string            
+
+  | If of (expr * expr * expr)            (* if-then-else *)
+  | Binop of (bop * expr * expr)          (* operação binária *)
+  | Wh of (expr * expr)                   (* while *)
+  | Asg of (expr * expr)                  (* atribuição *)
+  | Let of (string * tipo * expr * expr)  (* nome, tipo, valor, corpo *)
+  | New of expr                           (* alocação de nova referência *)
+  | Deref of expr                         (* desreferenciar uma referência *)
+  | Unit                                  (* Referância à '()' *)
+  | Seq of expr * expr                    (* sequência *)
+  | Read                                  (* Read [Unit/()] *)
+  | Print of expr           
+
+  
+
+
+
+
+  
+
 
 (* ----------------------------------------------- *)
 (* Programa para calcular fatorial de um número    *)
 (* ----------------------------------------------- *)
 
-(* Condição do while: !z > 0 *)
+(* !z > 0 *)
 let cndwhi = Binop(Gt, Deref (Id "z"), Num 0)
-(* Interpretação: enquanto o valor de z for maior que zero *)
 
-(* Primeiro comando dentro do while: y := !y * !z *)
+(* y := !y * !z *)
 let asgny = Asg(Id "y", Binop(Mul, Deref (Id "y"), Deref (Id "z")))
-(* Interpretação: atualiza y multiplicando seu valor atual pelo de z *)
-
-(* Segundo comando: z := !z - 1 *)
+(* z := !z - 1 *)
 let asgnz = Asg(Id "z", Binop(Sub, Deref (Id "z"), Num 1))
-(* Interpretação: decrementa z em 1 *)
-
-(* Corpo do while: sequência de asgny e asgnz *)
+(* Executa a multiplicação e depois o decremento *)
 let bdwhi = Seq(asgny, asgnz)
-(* Interpretação: executa a multiplicação e depois o decremento *)
 
-(* Estrutura while completa: condição e corpo *)
-let whi = Wh(cndwhi, bdwhi)
 (* Enquanto !z > 0, executa o corpo *)
-
-(* Comando para imprimir o resultado: print(!y) *)
+let whi = Wh(cndwhi, bdwhi)
+(* imprime o valor atual de y *)
 let prt = Print(Deref (Id "y"))
-(* Interpretação: imprime o valor atual de y *)
-
 (* Sequência de comandos: executa o while e depois imprime o resultado *)
 let seq = Seq(whi, prt)
 
-(* Programa principal *)
-let fat = 
-  Let("x", TyInt, Read,               (* lê um número e armazena em x *)
-  Let("z", TyRef TyInt, New (Id "x"), (* cria referência z com valor de x *)
-  Let("y", TyRef TyInt, New (Num 1),  (* cria referência y com valor inicial 1 *)
-  seq)))                              (* executa a sequência: calcula fatorial e imprime *)
 
-(*--------------------------------------------------DESCRIÇÃO EM CÓDIGO--------------------------------------------------------------------            
-let  x: int     =  read() in 
-let  z: ref int = new x in 
-let  y: ref int = new 1 in 
-
-(while (!z > 0) (
-    y :=  !y * !z;
-    z :=  !z - 1);
-print (! y))     
------------------------------------------------------------------------------------------------------------------------------------------*)
+(* Outra forma de fazer a mesma coisa?*)
+(*let fat = 
+  Let("x", TyInt, Read,                     [let  x: int     =  read() in]
+  Let("z", TyRef TyInt, New (Id "x"),       [let  z: ref int = new x in  ]  
+  Let("y", TyRef TyInt, New (Num 1),        [let  y: ref int = new 1 in  ]  
+  seq)))                                    [(while (!z > 0) (
+                                                  y :=  !y * !z;
+                                                  z :=  !z - 1);
+                                                print (! y))             ]*)
 
   
     
